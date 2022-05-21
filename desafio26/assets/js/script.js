1b15a8742ee913cd848b570795e324082d7ac6f0
TiagoTNX <tiagotnx@gmail.com>
2022-05-21 05:33:51 -0300

    ScrollReveal

---
+++ desafio26/assets/js/script.js
@@ -0,0 +1,39 @@
+ScrollReveal().reveal(".header", { delay: 200, duration: 1000 });
+ScrollReveal().reveal(".sectionTitle", {
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
+ScrollReveal().reveal(".collection", {
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
+ScrollReveal().reveal(".artists", {
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
+ScrollReveal().reveal(".footer", {
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
+ScrollReveal().reveal(".artists__img", {
+    scale: 0.85,
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
+ScrollReveal().reveal(".collection__container > img", {
+    scale: 0.85,
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
+ScrollReveal().reveal(".sectionTitle > img", {
+    scale: 0.85,
+    delay: 200,
+    distance: "50px",
+    duration: 1000,
+});
